6911beb146a276aa6156d5eca5b91fa2840f89ed
James Huynh <james@rubify.com>
2016-01-07 10:26:16 +0800

    fix NameError: uninitialized constant DocsplitImagesJob::Sidekiq

--- lib/docsplit_images/docsplit_images_job.rb
+++ lib/docsplit_images/docsplit_images_job.rb
@@ -1,5 +1,7 @@
+require 'sidekiq'
+
 class DocsplitImages::DocsplitImagesJob
-  include Sidekiq::Worker
+  include ::Sidekiq::Worker
 
   def perform(class_name, id)
     object = class_name.constantize.find_by_id(id)

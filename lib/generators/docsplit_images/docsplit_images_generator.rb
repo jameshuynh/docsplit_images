fa3fe83e6cf77e3ea3e3029e73d1108bc5d82f1a
James Huynh <james@rubify.com>
2017-02-14 15:00:33 +0800

    0.2.5 add after_docspliting

--- lib/generators/docsplit_images/docsplit_images_generator.rb
+++ lib/generators/docsplit_images/docsplit_images_generator.rb
@@ -1,15 +1,19 @@
 require 'rails/generators/active_record'
 
 class DocsplitImagesGenerator < ActiveRecord::Generators::Base
-  argument :attachment_names, :required => true, :type => :array, :desc => "The names of the attachment(s) to add.",
-           :banner => "attachment_one attachment_two attachment_three ..."
+  argument :attachment_names,
+           required: true,
+           type: :array,
+           desc: "The names of the attachment(s) to add.",
+           banner: "attachment_one attachment_two attachment_three ..."
 
   def self.source_root
     @source_root ||= File.expand_path('../templates', __FILE__)
   end
 
   def generate_migration
-    migration_template "docsplit_images_migration.rb.erb", "db/migrate/#{docsplit_images_migration_file_name}"
+    migration_template "docsplit_images_migration.rb.erb",
+                       "db/migrate/#{docsplit_images_migration_file_name}"
   end
 
   protected
